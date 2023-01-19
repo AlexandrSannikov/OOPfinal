c0a24e15cc1d56fc2e42f5868c4d0062973e09d3
MrFedko <miklerst@mail.ru>
2023-01-19 17:32:48 +0300

    pet and pack animal classes

--- Animals/Animal.java
+++ Animals/Animal.java
@@ -44,9 +44,5 @@ public class Animal {
                 ", dateOfBirth = '" + dateOfBirth + '\'' +
                 '}';
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Animal("Der", "12.01.12"));
-    }
 }
 
