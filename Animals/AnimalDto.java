e30c27878b2511e43e84bf70f6519233bdc37637
MrFedko <miklerst@mail.ru>
2023-01-19 20:48:47 +0300

    Animal DTO, Animal ENUM

--- Animals/AnimalDto.java
+++ Animals/AnimalDto.java
@@ -1,4 +1,99 @@
 package Animals;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 public class AnimalDto {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d");
+    private int id;
+    private String  name,
+                    commands;
+    private AnimalEnum type;
+    private Date date_of_birth;
+
+    public AnimalDto(int id, String name, String type, ArrayList commands, String date){
+        setId(id);
+        setName(name);
+        setType(type);
+        setCommands(commands);
+        setDate_of_birth(date);
+    }
+
+    public AnimalDto(String name, String type, ArrayList commands, String date){
+        setName(name);
+        setType(type);
+        setCommands(commands);
+        setDate_of_birth(date);
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommands() {
+        return commands;
+    }
+
+    public void setCommands(ArrayList commands) {
+        this.commands = commands.toString();
+    }
+
+    public AnimalEnum getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        switch (type.toLowerCase()){
+            case ("cat"):
+                this.type = AnimalEnum.CAT;
+                break;
+            case ("dog"):
+                this.type = AnimalEnum.DOG;
+                break;
+            case ("hamster"):
+                this.type = AnimalEnum.HAMSTER;
+                break;
+            case ("horse"):
+                this.type = AnimalEnum.HORSE;
+                break;
+            case ("camel"):
+                this.type = AnimalEnum.CAMEL;
+                break;
+            case ("donkey"):
+                this.type = AnimalEnum.DONKEY;
+                break;
+            default:
+                System.out.println("Not recognized type of animal");
+
+        }
+
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        try {
+            sdf.parse(date_of_birth);
+            sdf.setLenient(false);
+            this.date_of_birth = Date.valueOf(date_of_birth);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("Not valid date of birth");
+        }
+    }
 }
