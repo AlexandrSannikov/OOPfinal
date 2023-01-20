63665d8bfa94007db0669b85ddd2d906ef7babe2
MrFedko <miklerst@mail.ru>
2023-01-20 10:46:37 +0300

    get animals from db

--- db/DBService.java
+++ db/DBService.java
@@ -2,25 +2,22 @@ package db;
 
 import Animals.AnimalDto;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 import static db.Conn.connect;
 
 public class DBService {
     private Connection connect;
-    public static String CREATE_TABLE =
-            "CREATE TABLE animals "
-                    + "(animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-                    + "animal_name TEXT NOT NULL,"
-                    + "animal_type TEXT NOT NULL,"
-                    + "commands TEXT,"
-                    + "date_of_birth DATE);";
 
     public void creatTable() throws SQLException, ClassNotFoundException {
+        String CREATE_TABLE =
+                "CREATE TABLE " + ConnectData.TABLE
+                        + " (animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+                        + "animal_name TEXT NOT NULL,"
+                        + "animal_type TEXT NOT NULL,"
+                        + "commands TEXT,"
+                        + "date_of_birth DATE);";
         this.connect = connect();
         try (Connection connection = this.connect) {
             Statement statement = connection.createStatement();
@@ -31,7 +28,7 @@ public class DBService {
     }
 
     public void insertAnimal(AnimalDto animalDto) throws SQLException, ClassNotFoundException {
-        String sql = "INSERT INTO animals (animal_name, animal_type, commands, date_of_birth) " +
+        String sql = "INSERT INTO " + ConnectData.TABLE + " (animal_name, animal_type, commands, date_of_birth) " +
                 "VALUES (?,?,?,?);";
         this.connect = connect();
         try (Connection connection = this.connect;
@@ -44,5 +41,50 @@ public class DBService {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public AnimalDto getAnimal(int id) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM " + ConnectData.TABLE +" WHERE animal_id = ?";
+        this.connect = connect();
+        try (Connection connection = this.connect;
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            ResultSet resultSet = pstmt.executeQuery();
+            AnimalDto animal = null;
+            while (resultSet.next()){
+                animal = new AnimalDto(resultSet.getInt("animal_id"),
+                        resultSet.getString("animal_name"),
+                        resultSet.getString("animal_type"),
+                        resultSet.getString("commands"),
+                        resultSet.getString("date_of_birth"));
+
+            }
+            return animal;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public ArrayList getAllAnimals() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM " + ConnectData.TABLE;
+        this.connect = connect();
+        try (Connection connection = this.connect;
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet resultSet = pstmt.executeQuery();
+            AnimalDto animal = null;
+            ArrayList result = new ArrayList<>();
+            while (resultSet.next()){
+                animal = new AnimalDto(resultSet.getInt("animal_id"),
+                        resultSet.getString("animal_name"),
+                        resultSet.getString("animal_type"),
+                        resultSet.getString("commands"),
+                        resultSet.getString("date_of_birth"));
+                result.add(animal);
+            }
+            return result;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
